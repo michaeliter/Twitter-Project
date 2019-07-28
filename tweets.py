f2d6b2c15c2267a2fd6501ed8cd7303b3e02df85
Dan Iter <daniter@stanford.edu>
2019-07-28 13:15:22 -0700

    fixed urls

--- tweets.py
+++ tweets.py
@@ -10,6 +10,7 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 
 CONFIG_FILE="./config.json"
+LOG_LEVEL="WARN"
 
 def mike_bs():
     # mail stuff
@@ -97,10 +98,11 @@ def parse_config(filename):
     return json.load(open(filename, "r"))
 
 def create_api(config):
-    api = twitter.Api(consumer_key=config['consumer_key'],
-                           consumer_secret=config['consumer_secret'],
-                           access_token_key=config['access_token_key'],
-                           access_token_secret=config['access_token_secret'])
+    api = twitter.Api( consumer_key=config['consumer_key'],
+                       consumer_secret=config['consumer_secret'],
+                       access_token_key=config['access_token_key'],
+                       access_token_secret=config['access_token_secret'],
+                       tweet_mode="extended")
     return api
 
 def get_last_day_tweets(api, user, now, max_tweets=100):
@@ -109,7 +111,8 @@ def get_last_day_tweets(api, user, now, max_tweets=100):
     oldest_id = None
     while found_oldest != True:
         tweets = api.GetUserTimeline(user_id=user.id, count=20, trim_user=True, exclude_replies=True, max_id=oldest_id)
-        # print("Found %d tweets for user %s" % (len(tweets), user.screen_name))
+        if LOG_LEVEL == 'DEBUG':
+            print("Found %d tweets for user %s" % (len(tweets), user.screen_name))
         for tweet in tweets:
             timestamp = parser.parse(tweet.created_at)
             diff = now - timestamp
@@ -119,16 +122,19 @@ def get_last_day_tweets(api, user, now, max_tweets=100):
             all_tweets.append(tweet)
             oldest_id = tweet.id
             if len(all_tweets) >= max_tweets:
-                print("Max tweets for user",  user.screen_name)
+                if LOG_LEVEL == 'DEBUG':
+                    print("Max tweets for user",  user.screen_name)
                 found_oldest = True
                 break
             if not found_oldest:
-                print("Many tweets for user", user.screen_name)
+                if LOG_LEVEL == 'DEBUG':
+                    print("Many tweets for user", user.screen_name)
     return all_tweets
 
 def fetch_tweets(api, users):
     user_tweets = {}
-    print("Fetching %d users" % len(users))
+    if LOG_LEVEL == 'DEBUG':
+        print("Fetching %d users" % len(users))
     for user in users:
         if user.screen_name in config['ignore_user']:
             continue
@@ -136,16 +142,19 @@ def fetch_tweets(api, users):
         user_tweets[user] = tweets
     return user_tweets
 
-def match_urls(text):
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+def match_urls(tweet):
+    urls = []
+    if tweet.urls:
+        for url in tweet.urls:
+            urls.append(url.expanded_url)
+    #urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
     return urls
 
 def extract_urls(user_tweets):
     annotated_urls = defaultdict(list)
     for user, tweets in user_tweets.items():
         for tweet in tweets:
-            text = tweet.text
-            urls = match_urls(text)
+            urls = match_urls(tweet)
             for url in urls:
                 annotated_urls[url].append(user)
     return annotated_urls
@@ -161,7 +170,7 @@ def pretty_print(sorted_obj):
 def sort_and_print(urls):
     arxiv = defaultdict(list)
     nonarxiv = defaultdict(list)
-    for url, urers in urls.items():
+    for url, users in urls.items():
         if "arxiv" in url:
             arxiv[url] = users
         else:
@@ -171,7 +180,7 @@ def sort_and_print(urls):
     ret = "Arxiv Links:\n"
     ret += pretty_print(sorted_arxiv)
     ret += "\n"
-    ret += "Non-Arxiv:"
+    ret += "Non-Arxiv:\n"
     ret += pretty_print(sorted_non_arxiv)
     return ret
 
@@ -182,7 +191,8 @@ if __name__ == "__main__":
     api = create_api(config)
     users = api.GetFriends()
     user_tweets = fetch_tweets(api, users)
-    print("Found %d tweets" % sum([len(tweets) for tweets in user_tweets.values()]))
+    if LOG_LEVEL == 'DEBUG':
+        print("Found %d tweets" % sum([len(tweets) for tweets in user_tweets.values()]))
     annotated_urls = extract_urls(user_tweets)
     pretty_print_urls = sort_and_print(annotated_urls)
     print(pretty_print_urls)
