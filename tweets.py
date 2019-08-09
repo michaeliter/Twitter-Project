ebb9617c680191216e40080ba58c01a136bf043c
Dan Iter <daniter@stanford.edu>
2019-08-09 11:09:58 -0700

    fixed spin forever bug

--- tweets.py
+++ tweets.py
@@ -122,6 +122,8 @@ def get_last_day_tweets(api, user, now, max_tweets=100):
         tweets = api.GetUserTimeline(user_id=user.id, count=20, trim_user=True, exclude_replies=True, max_id=oldest_id)
         if LOG_LEVEL == 'DEBUG':
             print("Found %d tweets for user %s" % (len(tweets), user.screen_name))
+        if len(tweets) == 0:
+          return all_tweets
         for tweet in tweets:
             timestamp = parser.parse(tweet.created_at)
             diff = now - timestamp
